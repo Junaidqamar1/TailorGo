1f61a1d47a801120adbd932273f537b9d97b07b3
md junaid qamar <junaidqamar8753@gmil.com>
2026-04-13 21:24:01 +0530

    responsive

--- src/pages/Nav.jsx
+++ src/pages/Nav.jsx
@@ -2,6 +2,12 @@ import React from 'react'
 import "../styles/nav.css"
 import logo from "../assets/logo.png"
 function Nav() {
+//   const toggle = document.querySelector(".menu-toggle");
+// const nav = document.querySelector(".nav-links");
+
+// toggle.addEventListener("click", () => {
+//   nav.classList.toggle("active");
+// });
   return (
 <nav class="navbar">
   <div class="nav-container max-w">
@@ -9,7 +15,8 @@ function Nav() {
       {/* <span class="logo-text">Tailor<span>Go</span></span> */}
       <img src={logo} alt="" />
     </div>
-    
+    <div class="menu-toggle">☰</div>
+
     <ul class="nav-links">
       <li><a href="#how-it-works">How it Works</a></li>
       <li><a href="#find-tailors">Find Tailors</a></li>
