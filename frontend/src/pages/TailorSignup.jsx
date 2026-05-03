b7dc02947eec37ab55ef2b74a8e6f044192f0cda
md junaid qamar <junaidqamar8753@gmil.com>
2026-05-03 12:58:04 +0530

    bug fixed

--- frontend/src/pages/TailorSignup.jsx
+++ frontend/src/pages/TailorSignup.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import "../styles/auth.css";
-import "../styles/tailorSignup.css";
+// import "../styles/tailorSignup.css";
 import tailorVideo from "../assets/tailorVideo.mp4";
 import { authApi } from "../lib/api";
 
